3b94ccf525f74adeb761a2705ad32f8aefe70853
sasha42 <sasha42@users.noreply.github.com>
2016-02-26 16:58:23 +0100

    File handling in settings and urls

--- SubmissionBox/settings.py
+++ SubmissionBox/settings.py
@@ -122,4 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
+MEDIA_URL = '/files/'
+
 ADMIN_SITE_HEADER = "Submission Box"
--- SubmissionBox/urls.py
+++ SubmissionBox/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
 from . import settings
 
 admin.site.site_header = settings.ADMIN_SITE_HEADER
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
