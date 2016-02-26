a942947fba937bd4932c501b023b404c0757e307
sasha42 <sasha42@users.noreply.github.com>
2016-02-26 16:57:48 +0100

    Updated fields reflecting requirements

--- upload/models.py
+++ upload/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import uuid
 
 class Upload(models.Model):
-    description = models.TextField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.TextField(blank=True)
     file = models.FileField(upload_to='files/')
     capture_date = models.DateTimeField()
     capture_location = models.CharField(max_length=200)
     upload_date = models.DateTimeField(auto_now_add=True)
+    torchat_id = models.CharField(max_length=16)
